2c2a927381f17b13319c274680b4e9b8e9541bc1
sujith K S <kadursujith@gmail.com>
2026-02-11 23:36:36 +0530

    variable

---
+++ MultiVariableExample.java
@@ -0,0 +1,10 @@
+class MultiVariableExample{
+    public static void main(String[] args) {
+        int a=10;
+        int b=35;
+        int c=25;
+        System.out.println("a:" + a);
+        System.out.println("b:" + b);
+        System.out.println("c:" + c);
+    }
+}
