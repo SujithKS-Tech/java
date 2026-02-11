25c04a1eed965f687557c7d94ff94e3d4aee7a2c
sujith K S <kadursujith@gmail.com>
2026-02-11 23:19:15 +0530

    variable

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+
+        int age = 22;
+        String name = "Sujith";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
