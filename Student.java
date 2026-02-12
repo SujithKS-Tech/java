c62ee9e69fd88af0f29b1c1974c3d1f54c72ba6e
sujith K S <kadursujith@gmail.com>
2026-02-12 17:30:42 +0530

    variable

---
+++ Student.java
@@ -0,0 +1,11 @@
+class Student{
+    String name;
+    int age;
+    public static void main(String[] args) {
+        Student s1=new Student();
+        s1.name="sujith";
+        s1.age=22;
+        System.out.println(s1.name);
+        System.out.println(s1.age);
+    }
+}
---
+++ add.java
@@ -0,0 +1,8 @@
+class add{
+    public static void main(String []args){
+        int a=10;
+        int b=20;
+        int c=15;
+        System.out.print(a+b+c);
+    }
+}
