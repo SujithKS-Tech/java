7f38f25505f3f4a8dfd1500eeba6b5ae60e8b235
sujith K S <kadursujith@gmail.com>
2026-02-14 19:06:22 +0530

    variable

---
+++ datatypeexample.java
@@ -0,0 +1,17 @@
+public class datatypeexample {
+    public static void main(String[] args) {
+        int age=22;
+        String name="sujith k s";
+        double marks=88.5;
+        char grade='A';
+        boolean ispassed=true;
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(marks);
+        System.out.println(grade);
+        System.out.println(ispassed);
+
+
+    }
+    
+}
