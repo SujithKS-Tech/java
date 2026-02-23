a5444dbd00020166e2bcd3f8d7ef317cd45b6168
sujith K S <kadursujith@gmail.com>
2026-02-23 23:12:01 +0530

    variable

---
+++ Addition.java
@@ -0,0 +1,15 @@
+public class Addition{
+    int a=50;
+    int b=100;
+    void sum()
+    {
+    System.out.println(a+b);
+}
+        public static void main (String [] args){
+
+        Addition obj1 = new Addition();
+        obj1.sum();
+       
+    }
+}
+
---
+++ narrowcastingdemo.java
@@ -0,0 +1,6 @@
+public class narrowcastingdemo{
+    public static void main(String [] args){
+        byte num=(byte)257;
+        System.out.println(num);
+    }
+}
