b42fb5681874bc89d3444674c5a666d29e9612d4
sujith K S <kadursujith@gmail.com>
2026-02-23 17:36:07 +0530

    variable

---
+++ mobile.java
@@ -0,0 +1,23 @@
+public class mobile{
+    void iphone()
+    {
+    System.out.println("this is iphone");
+}
+    void android()
+    {
+        System.out.println("this is samsung");
+        }
+        void laptop()
+        {
+            System.out.println("this is windows");
+        }
+        public static void main (String [] args){
+
+        mobile obj1 = new mobile();
+        obj1.android();
+        obj1.iphone();
+        obj1.laptop();
+       
+    }
+}
+
