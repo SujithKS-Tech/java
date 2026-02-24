399be09fca9baf1e9164a08c5bb95146d01f0f51
sujith K S <kadursujith@gmail.com>
2026-02-24 17:15:45 +0530

    variable

--- narrowcastingdemo,java.java
+++
@@ -1,6 +0,0 @@
-public class narrowcastingdemo{
-    public static void main(String [] args){
-        byte num=(byte)257;
-        System.out.println(num);
-    }
-}
---
+++ operator1.java
@@ -0,0 +1,12 @@
+class operator1 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 3;
+
+        System.out.println(a + b);  
+        System.out.println(a - b);  
+        System.out.println(a * b);  
+        System.out.println(a / b);  
+    }
+}
