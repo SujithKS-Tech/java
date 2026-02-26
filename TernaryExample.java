0460e3b7023cc350987e0dd42e6b1125666c5a9a
sujith K S <kadursujith@gmail.com>
2026-02-26 20:12:31 +0530

    variable

---
+++ TernaryExample.java
@@ -0,0 +1,9 @@
+public class TernaryExample{
+    public static void main(String[] args) {
+        int a=20;
+        int b=40;
+
+        int max=(a>b)? a : b;
+        System.out.println("maximum :" +max);
+    }
+}
