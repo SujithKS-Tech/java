8120c906d37bcff47e2d129a33ed23b75f107fa0
sujith K S <kadursujith@gmail.com>
2026-03-13 19:29:15 +0530

    array

---
+++ pattern.java/p1.java
@@ -0,0 +1,17 @@
+class nestedforloopexample{
+    public static void main(String[] args) {
+        int n=5;
+        
+        for( int i=1; i<=n;i++){
+            for(int j=1; j<=n;j++){
+                if(j==1 || j==n || i==n){
+                    System.out.print("* ");
+                }
+                else{
+                     System.out.print( "  ");
+                }  
+            }
+            System.out.println();
+        }
+    }
+}
